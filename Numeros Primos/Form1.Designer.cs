213de1843698b126142ccc36de9f3b76f474edaa
Carlos-Daniel04 <carlosrijo04@icloud.com>
2026-02-25 22:42:51 -0400

    números primos

---
+++ Numeros Primos/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Numeros_Primos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblnumero = new System.Windows.Forms.Label();
+            this.txtnumero = new System.Windows.Forms.MaskedTextBox();
+            this.btnverificar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblnumero
+            // 
+            this.lblnumero.AutoSize = true;
+            this.lblnumero.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblnumero.Location = new System.Drawing.Point(38, 26);
+            this.lblnumero.Name = "lblnumero";
+            this.lblnumero.Size = new System.Drawing.Size(129, 20);
+            this.lblnumero.TabIndex = 0;
+            this.lblnumero.Text = "Ingrese un nuemero";
+            // 
+            // txtnumero
+            // 
+            this.txtnumero.Location = new System.Drawing.Point(29, 55);
+            this.txtnumero.Name = "txtnumero";
+            this.txtnumero.Size = new System.Drawing.Size(153, 26);
+            this.txtnumero.TabIndex = 1;
+            // 
+            // btnverificar
+            // 
+            this.btnverificar.Location = new System.Drawing.Point(62, 96);
+            this.btnverificar.Name = "btnverificar";
+            this.btnverificar.Size = new System.Drawing.Size(75, 29);
+            this.btnverificar.TabIndex = 2;
+            this.btnverificar.Text = "Verificar";
+            this.btnverificar.UseVisualStyleBackColor = true;
+            this.btnverificar.Click += new System.EventHandler(this.btnverificar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(264, 216);
+            this.Controls.Add(this.btnverificar);
+            this.Controls.Add(this.txtnumero);
+            this.Controls.Add(this.lblnumero);
+            this.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Numeros Primos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblnumero;
+        private System.Windows.Forms.MaskedTextBox txtnumero;
+        private System.Windows.Forms.Button btnverificar;
+    }
+}
+
---
+++ Numeros Primos/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Numeros_Primos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnverificar_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(txtnumero.Text, out int numero))
+            {
+                MessageBox.Show("Ingrese un número válido");
+                return;
+            }
+
+            if (numero <= 1)
+            {
+                MessageBox.Show("El número NO es primo");
+                return;
+            }
+
+            bool esPrimo = true;
+
+            for (int i = 2; i <= Math.Sqrt(numero); i++)
+            {
+                if (numero % i == 0)
+                {
+                    esPrimo = false;
+                    break;
+                }
+            }
+
+            if (esPrimo)
+            {
+                MessageBox.Show(" El número ES primo");
+            }
+            else
+            {
+                MessageBox.Show(" El número NO es primo");
+            }
+        }
+    }
+}
